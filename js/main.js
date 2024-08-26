703cbaa1086aa731b509786d214e66c16abd2fef
zulkifli1705 <148613182+zulkifli1705@users.noreply.github.com>
2024-08-26 21:09:34 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('Happy Birthday Ibu Anisa Kholifah Nurhasanah').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
